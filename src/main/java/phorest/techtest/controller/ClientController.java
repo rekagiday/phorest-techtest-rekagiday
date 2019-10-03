2139d182fcbe20a5cde31648a87a6c533bb98435
Reka Giday <giday.r@gmail.com>
2019-10-03 17:36:30 +0100

    Add Unirest library

--- src/main/java/phorest/techtest/controller/ClientController.java
+++ src/main/java/phorest/techtest/controller/ClientController.java
@@ -1,13 +1,33 @@
 package phorest.techtest.controller;
 
+import kong.unirest.GetRequest;
+import kong.unirest.Unirest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
 
 @Controller(value="/")
 public class ClientController {
 
     @GetMapping
     public String index(){
+        return "client";
+    }
+
+    @GetMapping(value = "/search")
+    public String searchClients(@RequestParam String email, @RequestParam String phone){
+        GetRequest query = Unirest.get("http://api-gateway-dev.phorest.com/third-party-api-server/api/business/")
+                .basicAuth(System.getenv("Username"), System.getenv("Password"));
+
+        if (!StringUtils.isEmpty(email)) {
+            query.queryString("email", email);
+        }
+        if (StringUtils.isEmpty(phone)) {
+            query.queryString("phone", phone);
+        }
+        query.asString();
+
         return "client";
     }
 }
---
+++ src/main/java/phorest/techtest/model/Client.java
@@ -0,0 +1,30 @@
+package phorest.techtest.model;
+
+public class Client {
+
+    private String clientId;
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String email;
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
