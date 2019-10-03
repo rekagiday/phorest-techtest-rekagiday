aa93e17a5a522641a21129044ac7917ccaa999f5
Reka Giday <giday.r@gmail.com>
2019-10-03 10:56:20 +0100

    Initial commit

---
+++ src/test/java/phorest/techtest/TechtestApplicationTests.java
@@ -0,0 +1,13 @@
+package phorest.techtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
